523e9dd01986ed82493e0cd2df54fb29fb4d1031
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-10-13 22:53:00 -0500

    Materialize initialized

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
-import M from 'materialize-css/dist/js/materialize.min.js'
+import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 
-const App=() => {
+const App = () => {
   useEffect(() => {
     // Init Materialize JS
     M.AutoInit();
-  })
-  return (
-    <div className="App">
-      My App
-    </div>
-  );
-}
+  });
+  return <div className='App'>My App</div>;
+};
 
 export default App;
