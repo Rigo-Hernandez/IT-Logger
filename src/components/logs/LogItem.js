8b2ebad1861adac82c4e5badb5ead7d49d4eed12
Rigo-Hernandez <hernandez_rigoberto@outlook.com>
2019-11-04 00:07:58 -0600

    added logItem component

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,17 +1,24 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const LogItem = ({ log }) => {
-    return (
-        <li className='collection-item'>
-            <div>
-                <a href='#edit-log-modal' className={`modal-trigger ${log.attention ? 'red-text' : 'blue-text'}`}>{log.message}</a>
-            </div>
-        </li>
-    )
-}
+  return (
+    <li className='collection-item'>
+      <div>
+        <a
+          href='#edit-log-modal'
+          className={`modal-trigger ${
+            log.attention ? 'red-text' : 'blue-text'
+          }`}
+        >
+          {log.message}
+        </a>
+      </div>
+    </li>
+  );
+};
 LogItem.propTypes = {
-    log: PropTypes.object.isRequired
-}
+  log: PropTypes.object.isRequired
+};
 
-export default LogItem
+export default LogItem;
