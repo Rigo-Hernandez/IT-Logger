079796b0d1adef0e16f40597cbb0fe952853b621
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-11-10 20:00:00 -0600

    Logitem & Premodal component initialized

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import LogItem from './LogItem';
+import Preloader from '../layout/Preloader'
 
 const Logs = () => {
   const [logs, setLogs] = useState([]);
@@ -20,11 +21,11 @@ const Logs = () => {
   };
 
   if (loading) {
-    return <h4>Loading...</h4>;
+    return <Preloader />;
   }
 
   return (
-    <ul className='collection-with-header'>
+    <ul className='collection with-header'>
       <li className='collection-header'>
         <h4 className='center'>System Logs</h4>
       </li>
