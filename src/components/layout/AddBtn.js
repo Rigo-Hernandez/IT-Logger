263e00e31b4a36b7250dde681885913a6af030b9
Rigo-Hernandez <hernandez_rigoberto@outlook.com>
2019-11-19 23:20:06 -0600

    Add technician button

--- src/components/layout/AddBtn.js
+++ src/components/layout/AddBtn.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 const AddBtn = () => {
-  return <div className='fixed-action-btn'>
-      <a href='#add-log-modal' className='btn-floating btn-large blue darken-2 modal-trigger'><i className='large material-icons'>add </i></a>
-  </div>;
+  return (
+    <div className='fixed-action-btn'>
+      <a
+        href='#add-log-modal'
+        className='btn-floating btn-large blue darken-2 modal-trigger'
+      >
+        <i className='large material-icons'>add </i>
+      </a>
+    </div>
+  );
 };
 
 export default AddBtn;
