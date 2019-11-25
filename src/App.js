46a944427be5e45720178ce1e2f83b566ac80851
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-11-24 23:10:51 -0600

    AddButton + AddLog modal initialized

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Fragment, useEffect } from 'react';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
-import AddBtn from './components/layout/AddBtn'
-import AddLogModal from './components/logs/AddLogModal'
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
@@ -12,7 +12,7 @@ const App = () => {
     // Initialize Materialize JS
     M.AutoInit();
   });
-  
+
   return (
     <Fragment>
       <SearchBar />
