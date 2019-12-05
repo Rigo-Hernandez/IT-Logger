6802d41efcf5b0c9e5e08b16c458dd6060a5c562
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-12-04 23:03:51 -0600

    Edit & Tech modal components initialized

--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-const AddLogModal = () => {
+const EditLogModal = () => {
   const [message, setMessage] = useState('');
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState('');
@@ -10,6 +10,8 @@ const AddLogModal = () => {
       M.toast({ html: 'Please enter a message and tech' });
     } else {
       console.log(message, tech, attention);
+
+      //Clear FIelds
       setMessage('')
       setTech('')
       setAttention(false)
@@ -17,7 +19,7 @@ const AddLogModal = () => {
   };
 
   return (
-    <div id='add-log-modal' className='modal' style={modalStyle}>
+    <div id='edit-log-modal' className='modal' style={modalStyle}>
       <div className='modal-content'>
         <h4>Enter System Log</h4>
         <div className='row'>
@@ -28,9 +30,6 @@ const AddLogModal = () => {
               value={message}
               onChange={e => setMessage(e.target.value)}
             />
-            <label htmlFor='message' className='active'>
-              Log Message
-            </label>
           </div>
         </div>
 
@@ -45,7 +44,6 @@ const AddLogModal = () => {
               <option value='' disabled>
                 Select Technician
               </option>
-              
             </select>
           </div>
         </div>
@@ -84,4 +82,4 @@ const modalStyle = {
   height: '75%'
 };
 
-export default AddLogModal;
+export default EditLogModal;
