f4635480f230e8252ee0339034da78fb692ede47
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-12-05 21:56:02 -0600

    Techitem and TechlistModal initialized

--- src/components/techs/TechListModal.js
+++ src/components/techs/TechListModal.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import TechItem from './TechItem'
+import TechItem from './TechItem';
 
 const TechListModal = () => {
   const [techs, setTechs] = useState([]);
@@ -20,20 +20,15 @@ const TechListModal = () => {
   };
 
   return (
-    <div id="tech-list-modal" className='modal'>
-        <div className= 'modal-content'>
-            <h4>Technician List</h4>
-            <ul className='collection'>
-                {!loading &&
-                techs.map(tech => (
-                <TechItem tech={tech} key={tech.id} />
-                ))}
-            </ul>
-
-        </div>
+    <div id='tech-list-modal' className='modal'>
+      <div className='modal-content'>
+        <h4>Technician List</h4>
+        <ul className='collection'>
+          {!loading &&
+            techs.map(tech => <TechItem tech={tech} key={tech.id} />)}
+        </ul>
+      </div>
     </div>
-
-
   );
 };
 
