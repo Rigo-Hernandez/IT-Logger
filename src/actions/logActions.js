1e59a51154cdbe29cb643733d198b15202252265
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2020-01-30 20:00:00 -0600

    Edit log modal initialized

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -3,7 +3,10 @@ import {
   SET_LOADING,
   LOGS_ERROR,
   ADD_LOG,
-  DELETE_LOG
+  DELETE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_LOG
 } from './types';
 
 //get logs from server
@@ -46,6 +49,48 @@ export const deleteLog = id => async dispatch => {
     });
   }
 };
+
+//Update log on server
+export const updateLog = log => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch(`/logs/${log.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(log),
+      header: {
+        'Content-Type' : 'application/json'
+      }
+    });
+
+    const data = await res.json()
+
+    dispatch({
+      type: UPDATE_LOG,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    });
+  }
+};
+
+
+// Set curreent log
+export const setCurrent = log => {
+  return {
+    type: SET_CURRENT,
+    payload: log
+  }
+}
+// Clear current log 
+export const clearCurrent = () => {
+  return {
+    type: CLEAR_CURRENT
+  }
+}
 
 //Add new log
 export const addLog = log => async dispatch => {
